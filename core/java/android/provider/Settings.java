4eaeedd5765ee9a873e02f015240b88dea066381
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 01:56:16 +0000

    Configurable 0, 90, 180 and 270 degree rotation
    Change-Id: Ia1859c51d71ef9d01cec2d13c6468ed89c6ac53e
    Contributor: jonasl
    
    - Add system setting
    
    Change-Id: Ia7bf8cb64258e1d602230a8f9ea227d3b56a4dab
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4417,6 +4417,12 @@ public final class Settings {
         public static final String SYSTEM_UI_THEME = "system_ui_theme";
 
         /**
+         * Allow all rotations.
+         * @hide
+         */
+        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
