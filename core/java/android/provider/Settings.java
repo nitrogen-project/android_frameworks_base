7eefd253cebc750bf7fe6afa9bc4e90fd41ddab0
Jmz <jmztaylor@gmail.com>
2014-01-06 15:30:15 -0500

    Force Expanded Notifications [1/2]
    This will force expanded notifications for all apps that support expanded notifications
    
    Change-Id: I2259ae02bbdeb0867b83a52277b5f7ff2de6485d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4117,6 +4117,12 @@ public final class Settings {
         public static final String DOZE_SCREEN_BRIGHTNESS = "doze_screen_brightness";
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
