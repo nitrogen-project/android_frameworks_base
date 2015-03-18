c663fff2e1ddd03be5ef46757f178a70bcc72fb2
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-03-19 00:52:48 +0100

    BatteryMeterDrawable: add dotted circle style (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8259,8 +8259,9 @@ public final class Settings {
          * Display style of the status bar battery information
          * 0: Display the battery an icon in portrait mode
          * 1: Display the battery as a circle
-         * 2: Display the battery as text
-         * 3: Do not display the battery
+         * 2: Display the battery as a dotted circle
+         * 3: Display the battery as text
+         * 4: Do not display the battery
          * default: 0
          * @hide
          */
@@ -8268,7 +8269,7 @@ public final class Settings {
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 3);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 4);
 
         /**
          * This are the settings to be backed up.
