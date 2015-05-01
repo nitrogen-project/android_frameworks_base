02e2f586a37bf9f56152574483126fb68ec0bc64
Christopher R. Palmer <crpalmer@gmail.com>
2015-05-01 06:15:47 -0400

    DreamManager: Expose doze state
    Previously the dream service would only report whether or not it was dreaming.
    Ambient display counts as dreaming.  When dreaming, the volume inputs are
    passed back to the application which is not what we want for ambient
    display.
    
    Fix this by exposing isDozing() and allow volume keys to control music
    when we are dreaming and have moved into doze mode (aka, sleeping).
    
    Change-Id: I3b1e72b95ec8b1b63ef0219259dfee77a5d339d3
    (cherry picked from commit 579f24d427758569a023650bdbe51fca290fedd6)
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/dreams/DreamManagerService.java
+++ services/core/java/com/android/server/dreams/DreamManagerService.java
@@ -241,6 +241,12 @@ public final class DreamManagerService extends SystemService {
         requestDreamInternal();
     }
 
+    private boolean isDozingInternal() {
+        synchronized (mLock) {
+            return mCurrentDreamIsDozing;
+        }
+    }
+
     private void requestDreamInternal() {
         // Ask the power manager to nap.  It will eventually call back into
         // startDream() if/when it is appropriate to start dreaming.
@@ -755,6 +761,17 @@ public final class DreamManagerService extends SystemService {
             }
         }
 
+        @Override // Binder call
+        public boolean isDozing() {
+            checkPermission(android.Manifest.permission.READ_DREAM_STATE);
+
+            final long ident = Binder.clearCallingIdentity();
+            try {
+                return isDozingInternal();
+            } finally {
+                Binder.restoreCallingIdentity(ident);
+            }
+        }
 
         @Override // Binder call
         public void dream() {
@@ -887,6 +904,11 @@ public final class DreamManagerService extends SystemService {
         @Override
         public void requestDream() {
             requestDreamInternal();
+        }
+
+        @Override
+        public boolean isDozing() {
+            return isDozingInternal();
         }
     }
 
