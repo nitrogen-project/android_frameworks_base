be7792174f5716f543bfadb2a19bfc59d5b23d0c
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4417,6 +4417,12 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
