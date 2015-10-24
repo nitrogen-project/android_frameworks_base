3f73aedfc761012e730121bdf98db7b4651972c9
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-23 23:44:43 -0400

    [1/2] Base: Hide power menu on secure lockscreen Adds an option to enable/disable power menu on secure lockscreen.
    PureNexus Edits:
    *made multiuser friendly

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4189,6 +4189,13 @@ public final class Settings {
          */
         public static final String LOCK_SCREEN_VISIBLE_NOTIFICATIONS = "lock_screen_visible_notifications";
 
+
+        /**
+         * This preference enables showing the power menu on LockScreen.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
