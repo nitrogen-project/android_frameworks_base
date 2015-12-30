429c5b0c0f32be6ad4b35a8a277c67fa52ccbdc9
qiushihan <hanqiushitju@gmail.com>
2015-12-30 11:01:30 +0800

    Cancel unlock sound when screen if off
    If window has WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD flag,
    when press power, there will be at least two sounds came out.
    
    There is no need to play unlock sound when screen is off, just cancel
    it.
    
    Google issue:
    https://code.google.com/p/android/issues/detail?id=197877
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1751,7 +1751,7 @@ public class KeyguardViewMediator extends SystemUI {
 
             // only play "unlock" noises if not on a call (since the incall UI
             // disables the keyguard)
-            if (TelephonyManager.EXTRA_STATE_IDLE.equals(mPhoneState)) {
+            if (mDeviceInteractive && TelephonyManager.EXTRA_STATE_IDLE.equals(mPhoneState)) {
                 playSounds(false);
             }
 
