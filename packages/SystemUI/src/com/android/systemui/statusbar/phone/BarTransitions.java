1a24e36397bf0aff37b4e49cf1de402def9750d4
bigrushdog <randall.rushing@gmail.com>
2016-08-26 14:48:07 -0700

    DUI: Initial DUI checkin for N
    -- Take ScreenPinning back to a more AOSP style
       implementation. Credit @Slimroms for the
       ScreenPinningRequest callbacks idea
    
    -- Scrap Car package.
    
    -- cleaner bar size math
    
    -- Handle density configuration changes
    
    Change-Id: I8f7a0997f60f66928fc0d25b8df71d3e23b611c9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
@@ -36,6 +36,7 @@ import android.view.View;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.omni.AbstractBatteryView;
+import com.android.systemui.navigation.NavigationController.NavbarOverlayResources;
 
 public class BarTransitions {
     private static final boolean DEBUG = false;
@@ -83,6 +84,8 @@ public class BarTransitions {
             mBarBackground.setWarningColor(color);
         }
     }
+
+    public void updateResources(NavbarOverlayResources resourceMap) {}
 
     /**
      * @param alwaysOpaque if {@code true}, the bar's background will always be opaque, regardless
