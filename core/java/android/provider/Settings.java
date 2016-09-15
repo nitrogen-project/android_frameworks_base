31cbd53ff1f3444f2361f746eb819bd71ed6ebd9
xyyx <xyyx@mail.ru>
2016-09-15 20:26:54 +0800

    Ability to disable dashboards conditions (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3809,6 +3809,12 @@ public final class Settings {
         public static final String SHOW_TILE_SUMMARY = "show_tile_summary";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
