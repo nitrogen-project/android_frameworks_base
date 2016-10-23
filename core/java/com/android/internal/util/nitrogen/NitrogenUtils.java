83f1b89ee14f217ef1818726cac67d019ee16951
Roman Birg <romanbirg@gmail.com>
2016-10-23 06:43:28 +0200

    NitrogenUtils: Add isPackageInstalled boolean
    Change-Id: Ibadea83b68c012c08e47c3622ba96a37c8ddf1b9
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/java/com/android/internal/util/nitrogen/NitrogenUtils.java
+++ core/java/com/android/internal/util/nitrogen/NitrogenUtils.java
@@ -18,7 +18,9 @@ package com.android.internal.util.nitrogen;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.hardware.input.InputManager;
 import android.os.Handler;
 import android.os.Looper;
@@ -116,6 +118,23 @@ public class NitrogenUtils {
                 }
             }
         }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+         return true;
+    }
+     public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 
     /**
