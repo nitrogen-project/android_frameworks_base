9a74d93952f1e8094d8c56b9c5dd0b820a3f7cf9
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-11-30 22:09:18 +0800

    [2/2] frameworks: show charging current & voltage on lockscreen Thanx to ezio84 for the switch ;)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3876,6 +3876,12 @@ public final class Settings {
          */
         public static final String KEY_MENU_ACTION = "key_menu_action";
 
+          /**
+          * Whether to show the negociated charger current in the lockscreen
+          * @hide
+          */
+         public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+ 
         /**
          * Action to perform when the menu key is long-pressed.
          * (Default is 0 on devices with a search key, 3 on devices without)
