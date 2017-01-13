3ec8c5969c60850378d93d9b18f6e9e4c752cd51
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2017-01-13 09:46:51 +0800

    Framework: Forward port ADB over network [1/3] Includes: - ADB Over Network, integration of the adb-host mode (already present)   The feature can be used/tested without the Settings part:
        setprop service.adb.tcp.port 5555
    
      Note: This ADB setting is not persistent (for security purpose) and
      require init.rc implementation event like this : http://bit.ly/AdbTcpIP
    
      Author: Tanguy Pruvot
      Id: I5c61a53948349c785356cb5aae165110d75e3074
    
      Author: sssemil <suleymanovemil8@gmail.com>
      Show notification on adb over network too
    
      Screenshots - http://goo.gl/TgsRI6
      Id: I9ddc0aa9a4f330a06ab5d97a8645d1b31bb6f299
    
      Author: Ethan Chen <intervigil@gmail.com>
      Use custom ADB over wifi property
      * Use a custom system property to trigger the real one, so we avoid
      running afoul of any SELinux CTS requirements.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources.Theme;
+import android.database.ContentObserver;
 import android.os.BaseBundle;
 import android.os.Build;
 import android.os.Environment;
@@ -223,6 +224,19 @@ public final class SystemServer {
     public SystemServer() {
         // Check for factory test mode.
         mFactoryTestMode = FactoryTest.getMode();
+    }
+
+    private class AdbPortObserver extends ContentObserver {
+        public AdbPortObserver() {
+            super(null);
+        }
+        @Override
+        public void onChange(boolean selfChange) {
+            int adbPort = Settings.Secure.getInt(mContentResolver,
+                Settings.Secure.ADB_PORT, 0);
+            // setting this will control whether ADB runs on TCP/IP or USB
+            SystemProperties.set("adb.network.port", Integer.toString(adbPort));
+        }
     }
 
     private void run() {
@@ -1182,6 +1196,15 @@ public final class SystemServer {
               mSystemServiceManager.startService(WEAR_TIME_SERVICE_CLASS);
           }
         }
+
+        // make sure the ADB_ENABLED setting value matches the secure property value
+        Settings.Secure.putInt(mContentResolver, Settings.Secure.ADB_PORT,
+                Integer.parseInt(SystemProperties.get("service.adb.tcp.port", "-1")));
+
+        // register observer to listen for settings changes
+        mContentResolver.registerContentObserver(
+            Settings.Secure.getUriFor(Settings.Secure.ADB_PORT),
+            false, new AdbPortObserver());
 
         // Before things start rolling, be sure we have decided whether
         // we are in safe mode.
