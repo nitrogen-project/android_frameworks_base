1edbae73455f2cf5622c6786b79885932b91d6a4
ezio84 <brabus84@gmail.com>
2017-01-15 13:42:59 +0800

    DUI Smartbar: double tap to sleep on navbar [1/3] this is more reliable than double tap to sleep action on navbar buttons

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4488,6 +4488,12 @@ public final class Settings {
         public static final String DND_WHEN_CALL = "dnd_when_call";
 
         /**
+         *  Enable smartbar double tap to sleep gesture
+         * @hide
+         */
+        public static final String SMARTBAR_DOUBLETAP_SLEEP = "smartbar_doubletap_sleep";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
