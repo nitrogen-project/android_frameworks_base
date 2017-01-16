5ff5156c3f0c021c3422e410ab7f5e59d7077773
ezio84 <brabus84@gmail.com>
2017-01-16 20:12:35 +0800

    DUI: Fix navbar edit non-protected broadcast logspam 01-14 13:51:31.585  5276  6478 E ActivityManager: Sending non-protected broadcast intent_navbar_edit from system 23657:com.android.settings/1000 pkg com.android.settings

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4486,7 +4486,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                         || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
                         || actionName.startsWith("eu.chainfire.supersu.NativeAccess")
                         || actionName.startsWith("qualcomm.intent.action.ACTION_AUDIO_STATE_CHANGED")
-                        || actionName.startsWith("com.qualcomm.intent.action.ACTION_UNSOL_RESPONSE_OEM_HOOK_RAW")) {
+                        || actionName.startsWith("com.qualcomm.intent.action.ACTION_UNSOL_RESPONSE_OEM_HOOK_RAW")
+                        || actionName.startsWith("intent_navbar_edit")) {
                     return true;
                 }
             }
