6fb6bd7451b91baccd00399c1a40d760c1e4abd0
maxwen <max.weninger@gmail.com>
2017-01-25 09:20:35 +0800

    base: SystemUI: update qs settings on dpi change same as for header images after the header has been recreated all the settings must be applied again to the new instance

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1163,6 +1163,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mStatusBarHeaderMachine.addObserver((QuickStatusBarHeader) mHeader);
                     mStatusBarHeaderMachine.updateEnablement();
 
+                    // and make sure all other settings are also applied again
+                    mHeader.updateSettings();
+
                     initSignalCluster(mHeader);
                     mHeader.setActivityStarter(PhoneStatusBar.this);
                     mStackScroller.updateNotificationView();
