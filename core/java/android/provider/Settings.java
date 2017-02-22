1009165583fb0ecd4b5c33c3bfa8f8d8266a0235
ezio84 <brabus84@gmail.com>
2017-02-22 19:26:41 +0100

    Pulse: allow custom opacity for solid line renderer [2/3]
    Signed-off-by: xyyx <xyyx@mail.ru>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6091,6 +6091,12 @@ public final class Settings {
          * @hide
          */
         public static final String PULSE_SOLID_UNITS_COUNT = "pulse_solid_units_count";
+
+        /**
+         * Pulse Solid units opacity
+         * @hide
+         */
+        public static final String PULSE_SOLID_UNITS_OPACITY = "pulse_solid_units_opacity";
  
         /**
          * A flag containing settings used for biometric weak
