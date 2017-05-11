9b0da1c3c3e8f04a37cc2f78efefec3cccf96577
xyyx <xyyx@mail.ru>
2017-05-11 19:28:16 +0800

    Revert "Proximity speaker [1/2]"
    This reverts commit a05a0575228b87bb27c64b35c278ef9958af78a4.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4328,25 +4328,6 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
-         * Whether the proximity sensor will adjust call to speaker
-         * @hide
-         */
-        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
-
-        /**
-         * Time delay to activate speaker after proximity sensor triggered
-         * @hide
-         */
-        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
-
-        /**
-         * Whether the proximity sensor will adjust call to speaker,
-         * only while in call (not while ringing on outgoing call)
-         * @hide
-         */
-        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY = "proximity_auto_speaker_incall_only";
-
-        /**
          * Whether to use the custom status bar header or not
          * @hide
          */
