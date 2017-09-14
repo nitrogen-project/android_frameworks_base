2fe10edeb8eefbf071f621b7976fc0dbcbb00b2e
Aaron Echols <atechols@gmail.com>
2017-09-14 22:19:25 +0800

    framework: port IME selector notification toggle (2/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4196,6 +4196,12 @@ public final class Settings {
         public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
 
         /**
+         * Whether to show the IME switcher in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
