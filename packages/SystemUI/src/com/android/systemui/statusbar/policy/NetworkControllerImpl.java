ffd583ca509b044f9da2b3ed60b129983b6708c1
Rashed97 <rashedabdeltawab@gmail.com>
2017-09-15 09:05:35 +0800

    SystemUI: Statusbar: Add HSPA+ icons Forward! Forward port H+ icon in the status bar when connected to HSPA+ Uses new vector drawables by blunden Migrated some code due to this split up: android/platform_frameworks_base@da68f59
    PS1: Initial commit
    PS2: New vectors (thanks blunden)
    PS3: Cleanup
    PS4: Use correct indentation (oops)
    PS5: Show correct icon in status bar (thanks mikeNG)
    PS6: Remove leftover Holo icons (thanks mikeNG)
    PS7: Rebase to depend on Holo icon removal (thanks mikeNG)
    PS8: Update commit message

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -865,6 +865,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
                             datatype.equals("e") ? TelephonyIcons.E :
                             datatype.equals("g") ? TelephonyIcons.G :
                             datatype.equals("h") ? TelephonyIcons.H :
+                            datatype.equals("hp") ? TelephonyIcons.HP :
                             datatype.equals("lte") ? TelephonyIcons.LTE :
                             datatype.equals("lte+") ? TelephonyIcons.LTE_PLUS :
                             datatype.equals("dis") ? TelephonyIcons.DATA_DISABLED :
@@ -968,6 +969,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean show4gForLte = false;
         boolean hideLtePlus = false;
         boolean hspaDataDistinguishable;
+	boolean hspapDataDistinguishable;
 
         static Config readConfig(Context context) {
             Config config = new Config();
@@ -980,6 +982,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
             config.hspaDataDistinguishable =
                     res.getBoolean(R.bool.config_hspa_data_distinguishable);
             config.hideLtePlus = res.getBoolean(R.bool.config_hideLtePlus);
+            config.hspapDataDistinguishable =
+                    res.getBoolean(R.bool.config_hspap_data_distinguishable);
             return config;
         }
     }
