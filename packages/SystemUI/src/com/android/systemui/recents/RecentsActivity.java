b89bac113c7bbff600a6b7a0b144114f0583317e
Leif Martensson <leif.martensson@sonymobile.com>
2017-09-20 08:20:01 +0200

    Fix empty recents view in multi window mode
    An empty recents view will be launched if recents become
    visible and another visible task is the resumed (top) task.
    
    Fix this by reloading tasks in onStart.
    
    Bug: 67627313
    Test: manual with test application in bug above
    
    Change-Id: Ibb6d80e498ccfbb1e9ece236b32b79b42b2058a3
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -392,6 +392,10 @@ public class RecentsActivity extends Activity implements ViewTreeObserver.OnPreD
 
         // Notify of the next draw
         mRecentsView.getViewTreeObserver().addOnPreDrawListener(mRecentsDrawnEventListener);
+
+        if (isInMultiWindowMode()) {
+            reloadTaskStack(true /* isInMultiWindowMode */, false /* sendConfigChangedEvent */);
+        }
     }
 
     @Override
