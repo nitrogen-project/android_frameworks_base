183ff6a9ac88e4f1ff694ec2e27472db8efab582
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-10-05 15:41:52 +0200

    Fix SystemUI FC after disabling navbar and unlocking the phone
    to replicate: boot with navbar enabled, then disable navbar, then switch
    the screen off, then unlock with fingeprint or power button
    
    java.lang.IllegalArgumentException: View=com.android.systemui.statusbar.phone.NavigationBarFrame{e0f4d97 G.E...... ......ID 0,0-1080,129 #7f0a01c4 app:id/navigation_bar_frame} not attached to window manager
    
    Change-Id: I5ae631c129af28615d26754848915e351970e422
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -7410,8 +7410,11 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         } else {
             if (mNavigationBarView != null){
+                FragmentHostManager fm = FragmentHostManager.get(mNavigationBarView);
                 mWindowManager.removeViewImmediate(mNavigationBarView);
                 mNavigationBarView = null;
+                fm.getFragmentManager().beginTransaction().remove(mNavigationBar).commit();
+                mNavigationBar = null;
             }
         }
     }
