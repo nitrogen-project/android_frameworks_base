52e0c8a74746131d3c58ba39aa907eb25121097f
xyyx <xyyx@mail.ru>
2017-12-15 20:29:26 +0800

    Add roaming icon to statusbar blacklist

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -67,6 +67,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     private static final String SLOT_ETHERNET = "ethernet";
     private static final String SLOT_VPN = "vpn";
     private static final String SLOT_VOLTE = "volte";
+    private static final String SLOT_ROAMING = "roaming";
 
     private final NetworkController mNetworkController;
     private final SecurityController mSecurityController;
@@ -122,6 +123,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     private boolean mForceBlockWifi;
     private boolean mBlockVolte;
     private boolean mBlockVpn;
+    private boolean mBlockRoaming;
 
     private final IconLogger mIconLogger = Dependency.get(IconLogger.class);
 
@@ -177,14 +179,17 @@ public class SignalClusterView extends LinearLayout implements NetworkController
         boolean blockEthernet = blockList.contains(SLOT_ETHERNET);
         boolean blockVolte = blockList.contains(SLOT_VOLTE);
         boolean blockVpn = blockList.contains(SLOT_VPN);
+        boolean blockRoaming = blockList.contains(SLOT_ROAMING);
 
         if (blockAirplane != mBlockAirplane || blockMobile != mBlockMobile
-                || blockEthernet != mBlockEthernet || blockWifi != mBlockWifi || blockVolte != mBlockVolte) {
+                || blockEthernet != mBlockEthernet || blockWifi != mBlockWifi || blockVolte != mBlockVolte
+                || blockRoaming != mBlockRoaming) {
             mBlockAirplane = blockAirplane;
             mBlockMobile = blockMobile;
             mBlockEthernet = blockEthernet;
             mBlockWifi = blockWifi || mForceBlockWifi;
             mBlockVolte = blockVolte;
+            mBlockRoaming = blockRoaming;
             // Re-register to get new callbacks.
             mNetworkController.removeCallback(this);
             mNetworkController.addCallback(this);
@@ -736,7 +741,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
                         (mMobileVisible ? "VISIBLE" : "GONE"), mMobileStrengthId, mMobileTypeId));
 
             mMobileType.setVisibility(mMobileTypeId != 0 ? View.VISIBLE : View.GONE);
-            mMobileRoaming.setVisibility(mRoaming ? View.VISIBLE : View.GONE);
+            mMobileRoaming.setVisibility(mRoaming && !mBlockRoaming ? View.VISIBLE : View.GONE);
             mMobileActivityIn.setVisibility(mActivityIn ? View.VISIBLE : View.GONE);
             mMobileActivityOut.setVisibility(mActivityOut ? View.VISIBLE : View.GONE);
 
