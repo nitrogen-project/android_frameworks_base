7da865c363b4d393fc65dd75624b915f1eca9d00
xyyx <xyyx@mail.ru>
2018-02-12 09:57:28 +0800

    Themes: build as apps * nexus and pixel ready

--- packages/overlays/DuiDarkThemeOverlay/Android.mk
+++ packages/overlays/DuiDarkThemeOverlay/Android.mk
@@ -1,14 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_CERTIFICATE := platform
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
-LOCAL_RRO_THEME := DuiDarkTheme
-
+LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := DuiDarkThemeOverlay
-
-include $(BUILD_RRO_PACKAGE)
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)
