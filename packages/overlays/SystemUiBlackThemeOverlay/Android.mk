9097eabf236bb1ee34bdb35b0313e7b5a349ae4e
xyyx <xyyx@mail.ru>
2018-02-20 11:56:26 +0800

    Rename themes

--- packages/overlays/SystemUiBlackThemeOverlay/Android.mk
+++ packages/overlays/SystemUiBlackThemeOverlay/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := DuiDarkThemeOverlay
+LOCAL_PACKAGE_NAME := SystemUiBlackThemeOverlay
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
