4fa57256f3309abfe5799cc3f481d3e728da3de4
ezio84 <brabus84@gmail.com>
2018-04-04 10:39:22 +0200

    QS: add right/left padding even if it's 0 for collapsed statusbar
    For devices without a rounded display we are setting rounded_corner_content_padding
    to 0 to avoid useless left/right padding in the statusbar.
    But this looks odd in the expanded statusbar / qs panel because other elements
    like the brightness slider buttons or the date have the padding anyway.
    
    We can handle those cases separately.
    
    Thanks @anirudhgupta109 for the suggestion.
    
    Change-Id: I77d6f10a8fb0c5a529f43ea6311bc98e37815106
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/RoundedCorners.java
+++ packages/SystemUI/src/com/android/systemui/RoundedCorners.java
@@ -53,9 +53,9 @@ public class RoundedCorners extends SystemUI implements Tunable {
     private View mOverlay;
     private View mBottomOverlay;
     private float mDensity;
-    private TunablePadding mQsPadding;
+    /*private TunablePadding mQsPadding;
     private TunablePadding mStatusBarPadding;
-    private TunablePadding mNavBarPadding;
+    private TunablePadding mNavBarPadding;*/
 
     @Override
     public void start() {
@@ -66,9 +66,9 @@ public class RoundedCorners extends SystemUI implements Tunable {
         }
         int padding = mContext.getResources().getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding);
-        if (padding != 0) {
-            setupPadding(padding);
-        }
+        int qsPadding = mContext.getResources().getDimensionPixelSize(
+                R.dimen.qs_corner_content_padding);
+        setupPadding(padding, qsPadding);
     }
 
     private void setupRounding() {
@@ -131,19 +131,23 @@ public class RoundedCorners extends SystemUI implements Tunable {
         });
     }
 
-    private void setupPadding(int padding) {
+    private void setupPadding(int padding, int qsPadding) {
         // Add some padding to all the content near the edge of the screen.
         StatusBar sb = getComponent(StatusBar.class);
         View statusBar = (sb != null ? sb.getStatusBarWindow() : null);
         if (statusBar != null) {
-            TunablePadding.addTunablePadding(statusBar.findViewById(R.id.keyguard_header), PADDING,
-                    padding, FLAG_END);
-
             FragmentHostManager fragmentHostManager = FragmentHostManager.get(statusBar);
-            fragmentHostManager.addTagListener(CollapsedStatusBarFragment.TAG,
-                    new TunablePaddingTagListener(padding, R.id.status_bar));
-            fragmentHostManager.addTagListener(QS.TAG,
-                    new TunablePaddingTagListener(padding, R.id.header));
+            if (padding != 0) {
+                TunablePadding.addTunablePadding(statusBar.findViewById(R.id.keyguard_header), PADDING,
+                        padding, FLAG_END);
+
+                fragmentHostManager.addTagListener(CollapsedStatusBarFragment.TAG,
+                        new TunablePaddingTagListener(padding, R.id.status_bar));
+            }
+            if (qsPadding != 0) {
+                fragmentHostManager.addTagListener(QS.TAG,
+                        new TunablePaddingTagListener(qsPadding, R.id.header));
+            }
         }
     }
 
