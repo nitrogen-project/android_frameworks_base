9d8cfe71ef13af93872ac8c121be8ddbeba20b74
SpiritCroc <dev@spiritcroc.de>
2018-11-26 18:29:32 +0100

    base: SystemUI: Restore AOSP QS style
    - slider thumb image
    - animation when not using bottom slider: partly revert
        10e118ce2911: Footer brightness slider: improve hide/show animation
        6c42d21df703: Revert "base: SystemUI: fix brightness slider animation on qs expand"
      to effectively restore behaviour of
        commit 9831eba618f65909ec7c4e52bfb1c61967d36a7e
        Author: maxwen <max.weninger@gmail.com>
        Date:   Thu Sep 6 13:15:50 2018 +0200
    
            base: SystemUI: fix brightness slider animation on qs expand
    
            when on bottom
    
            Change-Id: I94c761dda80a29ab17a7b08ffd1204a20a8fe0d
    - slider padding
    
    Change-Id: I751798caf2606c9cd28a165303b6586c111095c6
    Signed-off-by: Vol Zhdanov <wight554@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -101,6 +101,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private boolean mShowAutoBrightnessButton = false;
     private boolean mShowBrightnessSideButtons = false;
 
+    private boolean mBrightnessBottom;
+
     protected QSTileLayout mTileLayout;
 
     private QSCustomizer mCustomizePanel;
@@ -278,9 +280,11 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             if (newValue == null || Integer.parseInt(newValue) == 0) {
                 removeView(mBrightnessView);
                 addView(mBrightnessView, 0);
+                mBrightnessBottom = false;
             } else {
                 removeView(mBrightnessView);
                 addView(mBrightnessView, getBrightnessViewPositionBottom());
+                mBrightnessBottom = true;
             }
         }
         if (QS_SHOW_AUTO_BRIGHTNESS_BUTTON.equals(key)) {
@@ -824,5 +828,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     }
     private void setBrightnessMinMax(boolean min) {
         mBrightnessController.setBrightnessFromSliderButtons(min ? 0 : GAMMA_SPACE_MAX);
+    }
+
+    public boolean isBrightnessViewBottom() {
+        return mBrightnessBottom;
     }
 }
