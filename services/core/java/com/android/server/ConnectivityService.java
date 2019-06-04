2e342f1cea54dc744df1e2291468acf9f4c36917
phenomenall <venkysaihot@gmail.com>
2019-06-04 08:00:03 +0200

    Add missing ensureRunningOnConnectivityServiceThread() function *After June Patch Merge
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: xyyx <xyyx@mail.ru>

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -4167,7 +4167,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
     private BroadcastReceiver mUserIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
+	    final String action = intent.getAction();
             final int userId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, UserHandle.USER_NULL);
             if (userId == UserHandle.USER_NULL) return;
 
@@ -4656,6 +4656,15 @@ public class ConnectivityService extends IConnectivityManager.Stub
         return nai == getDefaultNetwork();
     }
 
+
+    private void ensureRunningOnConnectivityServiceThread() {
+        if (mHandler.getLooper().getThread() != Thread.currentThread()) {
+            throw new IllegalStateException(
+                    "Not running on ConnectivityService thread: "
+                            + Thread.currentThread().getName());
+        }
+    }
+
     private boolean isDefaultRequest(NetworkRequestInfo nri) {
         return nri.request.requestId == mDefaultRequest.requestId;
     }
@@ -5027,7 +5036,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
     }
 
     public void handleUpdateLinkProperties(NetworkAgentInfo nai, LinkProperties newLp) {
-        if (getNetworkAgentInfoForNetId(nai.network.netId) != nai) {
+	if (getNetworkAgentInfoForNetId(nai.network.netId) != nai) {
             // Ignore updates for disconnected networks
             return;
         }
