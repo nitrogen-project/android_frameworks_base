b441c18659007e0d4e7e563afcfd03b465d76e34
Sirhc95 <chris@cypheros.co>
2019-09-04 15:09:49 +0000

    Telephony: Conditionally ignore RSSNR signal level

--- telephony/java/android/telephony/SignalStrength.java
+++ telephony/java/android/telephony/SignalStrength.java
@@ -900,7 +900,15 @@ public class SignalStrength implements Parcelable {
                     + rsrpIconLevel + " snrIconLevel:" + snrIconLevel
                     + " lteRsrpBoost:" + mLteRsrpBoost);
 
-            /* Choose a measurement type to use for notification */
+               /* Ignore RSSNR for now, conditionally */
+        boolean rssnrIgnored = Resources.getSystem().getBoolean(
+                com.android.internal.R.bool.config_ignoreRssnrSignalLevel);
+        if (rssnrIgnored) {
+            // Ignore RSSNR
+            if (rsrpIconLevel != -1) return rsrpIconLevel;
+        }
+
+               /* Choose a measurement type to use for notification */
             if (snrIconLevel != -1 && rsrpIconLevel != -1) {
                 /*
                  * The number of bars displayed shall be the smaller of the bars
