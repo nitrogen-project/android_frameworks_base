4753294696b0e9a73ed3100d450c899c0da17601
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-13 18:35:50 -0300

    SystemUI: Hide back icon when screen pinned and gestural mode enabled
    Change-Id: I0d63e273b5b36361d84c274a8899493c099045b4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -640,6 +640,9 @@ public class NavigationBarView extends FrameLayout implements
         } else if (pinningActive) {
             disableBack = disableRecent = false;
         }
+        if (pinningActive && isGesturalMode(mNavBarMode)){
+            disableBack = true;
+        }
 
         ViewGroup navButtons = getCurrentView().findViewById(R.id.nav_buttons);
         if (navButtons != null) {
