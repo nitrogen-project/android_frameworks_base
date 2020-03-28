b0c3f13f8dbd4a47ed2b75f9c806572ded0ae4a8
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-28 12:51:07 -0300

    [1/2] Allow changing face unlock method when locked
    Change-Id: I9a8e812bbe869dead94d631fa0a2f53f3bf67cc6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11318,6 +11318,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
+         * Face Unlock Method
+         * @hide
+         */
+        public static final String FACE_UNLOCK_METHOD = "face_unlock_method";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
