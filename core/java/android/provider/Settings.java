ce0cbbee193dd16644782bb72cb3f60163f8caf8
DennySPb <dennyspb@gmail.com>
2020-06-22 16:52:51 +0300

    SystemUI: Optional VoWiFi icon [1/2]
    Change-Id: I637cbc81283175cdb092a4e70fda28cacde7238c
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5673,6 +5673,12 @@ public final class Settings {
         public static final String VOLTE_ICON_STYLE = "volte_icon_style";
 
         /**
+         * Whether to show VoWiFi icon or not
+         * @hide
+         */
+        public static final String VOWIFI_ICON = "vowifi_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
