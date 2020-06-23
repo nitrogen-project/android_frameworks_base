61106cb410bb2da77c982889fa2a9055d06080e3
DennySPb <dennyspb@gmail.com>
2020-06-23 14:34:56 +0300

    [SQUASH] SystemUI: Addd more selectable VoWiFi icons [1/2]
    - OnePlus
    - Asus
    - Huawei
    - Motorola
    
    Change-Id: I20c777891f0bf0ed8e65e50758012bd876bd41d5
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    Add three more selectable VoWiFi icons [1/2]
    
    from https://www.onlinewebfonts.com/
    
    Change-Id: Iadc2a5a5c87b1e6fb1fdcd48d4d1c3e7b87d6adb
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -132,6 +132,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     // VoWiFi Icon
     private int mVoWiFiIcon;
+    // VoWiFi Icon Style
+    private int mVoWiFistyle;
 
     private final MobileStatusTracker.Callback mMobileCallback =
             new MobileStatusTracker.Callback() {
@@ -316,6 +318,10 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
             resolver.registerContentObserver(Settings.System.getUriFor(
                    Settings.System.VOWIFI_ICON),
                    false,this, UserHandle.USER_ALL);
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.VOWIFI_ICON_STYLE),
+                  false,this, UserHandle.USER_ALL);
+           updateSettings();
         }
 
         /*
@@ -337,6 +343,9 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 UserHandle.USER_CURRENT);
         mVoWiFiIcon = Settings.System.getIntForUser(resolver,
                 Settings.System.VOWIFI_ICON, 0,
+                UserHandle.USER_CURRENT);
+        mVoWiFistyle = Settings.System.getIntForUser(resolver,
+                Settings.System.VOWIFI_ICON_STYLE, 0,
                 UserHandle.USER_CURRENT);
         mConfig = Config.readConfig(mContext);
         setConfiguration(mConfig);
@@ -1052,7 +1061,31 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         if ( isVowifiAvailable() && !isCallIdle() ) {
             return TelephonyIcons.VOWIFI_CALLING;
         }else if (isVowifiAvailable()) {
-            return TelephonyIcons.VOWIFI;
+            switch(mVoWiFistyle) {
+                // OOS
+                case 1:
+                    return TelephonyIcons.VOWIFI_ONEPLUS;
+                // Motorola
+                case 2:
+                    return TelephonyIcons.VOWIFI_MOTO;
+                // ASUS
+                case 3:
+                    return TelephonyIcons.VOWIFI_ASUS;
+                // EMUI (Huawei P10)
+                case 4:
+                    return TelephonyIcons.VOWIFI_EMUI;
+                // Simple1
+                case 5:
+                    return TelephonyIcons.VOWIFI_Simple1;
+                // Simple2
+                case 6:
+                    return TelephonyIcons.VOWIFI_Simple2;
+                // Simple3
+                case 7:
+                    return TelephonyIcons.VOWIFI_Simple3;
+                default:
+                    return TelephonyIcons.VOWIFI;
+            }
         }else {
             return null;
         }
