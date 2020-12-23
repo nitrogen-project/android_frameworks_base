c7fca30ef79bd6d3e2a89046d1d24b461bd2bb17
Volodymyr Zhdanov <wight554@gmail.com>
2020-12-23 18:31:05 +0100

    Revert "Network traffic: fix static color usage"
    * was needed for traffic icons it seems
    
    This reverts commit c75b5570d1adfa1cab56ba58987cb27f98d99d59.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -72,7 +72,6 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     private int mVisibleState = -1;
     private boolean mTrafficVisible = false;
     private boolean mSystemIconVisible = true;
-    private boolean mColorIsStatic = false;
 
     private boolean mScreenOn = true;
 
@@ -306,9 +305,6 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
 
     @Override
     public void onDarkChanged(Rect area, float darkIntensity, int tint) {
-        if (mColorIsStatic) {
-            return;
-        }
         mTintColor = DarkIconDispatcher.getTint(area, this, tint);
         setTextColor(mTintColor);
     }
@@ -358,7 +354,6 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
 
     @Override
     public void setStaticDrawableColor(int color) {
-        mColorIsStatic = true;
         mTintColor = color;
         setTextColor(mTintColor);
     }
