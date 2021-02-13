21f418ca88234f95bb1578472182212700452f9c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-13 21:21:13 +0530

    SystemUI: NetworkTraffic: Minor refinements
    - reduce side padding of statusbar indicator
    - show /S in caps
    - reduce overall text size and increase line spacing
    
    	SystemUI: Reduce statusbar network speed margins
    
    	Change-Id: I0d031048a5499508138f04f7be2ae12819d4ee69
    
    arrow-12.1: Correct values
    
    Change-Id: I9bc80ba00700c5a184cce2b6703d11986203739b

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -65,17 +65,17 @@ public class NetworkTrafficSB extends NetworkTraffic implements DarkReceiver, St
     @Override
     protected void setSpacingAndFonts() {
         setTextAppearance(R.style.TextAppearance_QS_Status);
-        setLineSpacing(0.75f, 0.75f);
+        setLineSpacing(0.83f, 0.83f);
     }
 
     @Override
     protected RelativeSizeSpan getSpeedRelativeSizeSpan() {
-        return new RelativeSizeSpan(0.75f);
+        return new RelativeSizeSpan(0.70f);
     }
 
     @Override
     protected RelativeSizeSpan getUnitRelativeSizeSpan() {
-        return new RelativeSizeSpan(0.7f);
+        return new RelativeSizeSpan(0.60f);
     }
 
     @Override
