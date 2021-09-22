124367329ff47805da38602d254f6b6a4596859d
Gurpreet Ghai <gghai@codeaurora.org>
2021-09-22 09:42:14 +0530

    BT: Add entry for MCP Service in Frameworks
    - Add Profile entry for MCP Service
    
    CRs-Fixed: 3033146
    Change-Id: I7f89b6ab2c84f8681d953c0f5c65750946962fc3
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: xyyx <xyyx@mail.ru>

--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -241,13 +241,19 @@ public interface BluetoothProfile {
      */
     public static final int CC_SERVER = 27;
 
+   /**
+     * MCP_SERVER
+     * @hide
+     */
+    public static final int MCP_SERVER = 28;
+
     /**
      * Max profile ID. This value should be updated whenever a new profile is added to match
      * the largest value assigned to a profile.
      *
      * @hide
      */
-    int MAX_PROFILE_ID = 27;
+    int MAX_PROFILE_ID = 28;
 
     /**
      * Default priority for devices that we try to auto-connect to and
