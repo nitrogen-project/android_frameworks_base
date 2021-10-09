d27d318ef92fe8c555493521802bed836f6a0fb5
Park Ju Hyung <qkrwngud825@gmail.com>
2021-10-09 03:22:39 -0400

    SystemUI: Implement burn-in protection for status/navbar
    Devices with OLED display suffer from
    status-bar's notification items and nagivation bar's software keys
    causing permanent burn-ins when used long-term.
    
    Moving all items in the area
    both horizontally and vertically workarounds this problem.
    
    This new feature can be enabled by setting
    config_statusBarBurnInProtection to true.
    
    The shifting interval can be configured with config_shift_interval.
    The default shifting interval is 60 seconds.
    
    Forward-ported to oreo-mr1.
    
    Change-Id: I8df1ebc8bc0f359fe5a6a1fe11aa6201237a7359
    
    SystemUI: rework statusbar burn-in protection controller
    
    * Turns out that this controller was instantiated twice resulting in two timers running simultaneously which resulted in views to shift abruptly.
      Since the shift amount was too low it was not noticeable at all. So now we instantiate it once with all final dependencies and inject PhoneStatusBarView in fragment transaction.
    * Finalized many instance variables and a reference to the main handler is kept instead of creating new ones in each cycle
    * simplified / generalised the shift algorithm a bit so that it's easily configurable
    * added a callback to reload shift vars on screen density changes
    * additional changes:
    * use the same controller for navigation handle, saves some cpu time
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    SystemUI: inject BurnInProtectionController
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]: use the scoped SysUISingleton annotation
    
    SystemUI: BurnInProtectionController: rewrite in kotlin and improvements
    * ditched TimerTask in favor of coroutines
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]:
    * adapted to A13
    * use BurnInHelper util functions for calculating offset
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I067022d43d20f43760a192b43569c31316a5337a
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -224,6 +224,7 @@ import com.android.systemui.statusbar.phone.panelstate.PanelExpansionChangeEvent
 import com.android.systemui.statusbar.phone.panelstate.PanelExpansionStateManager;
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
+import com.android.systemui.statusbar.policy.BurnInProtectionController;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
@@ -662,6 +663,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
     private final InteractionJankMonitor mJankMonitor;
 
+    private final BurnInProtectionController mBurnInProtectionController;
 
     /**
      * Public constructor for CentralSurfaces.
@@ -763,7 +765,8 @@ public class CentralSurfacesImpl extends CoreStartable implements
             DeviceStateManager deviceStateManager,
             DreamOverlayStateController dreamOverlayStateController,
             WiredChargingRippleController wiredChargingRippleController,
-            IDreamManager dreamManager) {
+            IDreamManager dreamManager,
+            BurnInProtectionController burnInProtectionController) {
         super(context);
         mNotificationsController = notificationsController;
         mFragmentService = fragmentService;
@@ -855,6 +858,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
         statusBarWindowStateController.addListener(this::onStatusBarWindowStateChanged);
 
         mScreenOffAnimationController = screenOffAnimationController;
+        mBurnInProtectionController = burnInProtectionController;
 
         mPanelExpansionStateManager.addExpansionListener(this::onPanelExpansionChanged);
 
@@ -885,6 +889,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
         deviceStateManager.registerCallback(mMainExecutor,
                 new FoldStateListener(mContext, this::onFoldedStateChanged));
         wiredChargingRippleController.registerCallbacks();
+        mBurnInProtectionController.setCentralSurfaces(this);
     }
 
     @Override
@@ -1174,6 +1179,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
                     mNotificationPanelViewController.updatePanelExpansionAndVisibility();
                     setBouncerShowingForStatusBarComponents(mBouncerShowing);
                     checkBarModes();
+                    mBurnInProtectionController.setPhoneStatusBarView(mStatusBarView);
                 });
         initializer.initializeStatusBar(mCentralSurfacesComponent);
 
@@ -3602,6 +3608,8 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
             updateNotificationPanelTouchState();
             mNotificationShadeWindowViewController.cancelCurrentTouch();
+
+            mBurnInProtectionController.stopShiftTimer();
             if (mLaunchCameraOnFinishedGoingToSleep) {
                 mLaunchCameraOnFinishedGoingToSleep = false;
 
@@ -3705,6 +3713,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
                 }
             }
             updateScrimController();
+            mBurnInProtectionController.startShiftTimer();
         }
     };
 
