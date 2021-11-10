9336e9b93e84b5b9cd2437d65efc39b23dbc5a5a
DennySPb <dennyspb@gmail.com>
2021-11-10 09:44:53 +0300

    ScreenOff torch: Allow to disable torch when screen is on
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I253815c7f1f988c20f4a8f041f9c66a811934b6f
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2346,7 +2346,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         void onLongPress(long eventTime) {
-            if (mSingleKeyGestureDetector.beganFromNonInteractive()) {
+            if (mSingleKeyGestureDetector.beganFromNonInteractive() ||
+                         (mTorchActionMode != 0 && isFlashLightIsOn())) {
                 if (handleTorchPress(true))
                     return;
                 if (!mSupportLongPressPowerWhenNonInteractive) {
@@ -2366,7 +2367,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         void onMultiPress(long downTime, int count) {
-            if (mSingleKeyGestureDetector.beganFromNonInteractive()) {
+            if (mSingleKeyGestureDetector.beganFromNonInteractive() ||
+                         (mTorchActionMode != 0 && isFlashLightIsOn())) {
                 if (handleTorchPress(false)) {
                     mSingleKeyGestureDetector.reset();
                     return;
@@ -2374,6 +2376,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             powerPress(downTime, count, mSingleKeyGestureDetector.beganFromNonInteractive());
         }
+    }
+
+    private boolean isFlashLightIsOn() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.FLASHLIGHT_ENABLED, 0) != 0;
     }
 
     public boolean handleTorchPress(boolean longpress) {
