618f9982ada49695758dfff71a80fbc0d93e4b58
xyyx <xyyx@mail.ru>
2022-03-10 14:40:19 +0300

    voWiFi: Remove isWide from MobileIconGroup

--- packages/SettingsLib/src/com/android/settingslib/mobile/TelephonyIcons.java
+++ packages/SettingsLib/src/com/android/settingslib/mobile/TelephonyIcons.java
@@ -362,8 +362,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI,
-            false);
+            TelephonyIcons.ICON_VOWIFI);
 
     public static final MobileIconGroup VOWIFI_CALLING = new MobileIconGroup(
             "VoWIFICall",
@@ -375,8 +374,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_CALLING,
-            false);
+            TelephonyIcons.ICON_VOWIFI_CALLING);
 
     public static final MobileIconGroup VOWIFI_MOTO = new MobileIconGroup(
             "VoWIFI_Moto",
@@ -388,8 +386,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_MOTO,
-            false);
+            TelephonyIcons.ICON_VOWIFI_MOTO);
 
     public static final MobileIconGroup VOWIFI_ASUS = new MobileIconGroup(
             "VoWIFI_ASUS",
@@ -401,8 +398,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_ASUS,
-            false);
+            TelephonyIcons.ICON_VOWIFI_ASUS);
 
     public static final MobileIconGroup VOWIFI_ONEPLUS = new MobileIconGroup(
             "VoWIFI_OnePlus",
@@ -414,8 +410,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_ONEPLUS,
-            false);
+            TelephonyIcons.ICON_VOWIFI_ONEPLUS);
 
     public static final MobileIconGroup VOWIFI_EMUI = new MobileIconGroup(
             "VoWIFI_Emui",
@@ -427,8 +422,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_EMUI,
-            false);
+            TelephonyIcons.ICON_VOWIFI_EMUI);
 
     public static final MobileIconGroup VOWIFI_Simple1 = new MobileIconGroup(
             "VoWIFI_Simple1",
@@ -440,8 +434,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_Simple1,
-            false);
+            TelephonyIcons.ICON_VOWIFI_Simple1);
 
     public static final MobileIconGroup VOWIFI_Simple2 = new MobileIconGroup(
             "VoWIFI_Simple2",
@@ -453,8 +446,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_Simple2,
-            false);
+            TelephonyIcons.ICON_VOWIFI_Simple2);
 
     public static final MobileIconGroup VOWIFI_Simple3 = new MobileIconGroup(
             "VoWIFI_Simple3",
@@ -466,8 +458,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_Simple3,
-            false);
+            TelephonyIcons.ICON_VOWIFI_Simple3);
 
     public static final MobileIconGroup VOWIFI_VIVO = new MobileIconGroup(
             "VoWIFI_VIVO",
@@ -479,8 +470,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_VIVO,
-            false);
+            TelephonyIcons.ICON_VOWIFI_VIVO);
 
     public static final MobileIconGroup VOWIFI_Margaritov = new MobileIconGroup(
             "VoWIFI_Syberia",
@@ -492,8 +482,7 @@ public class TelephonyIcons {
             0,
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
-            TelephonyIcons.ICON_VOWIFI_Margaritov,
-            false);
+            TelephonyIcons.ICON_VOWIFI_Margaritov);
 
 
     // When adding a new MobileIconGround, check if the dataContentDescription has to be filtered
