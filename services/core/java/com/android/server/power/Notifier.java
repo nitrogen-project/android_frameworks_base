1a3fc2cccc3be7726a8f7a622e96bec3d403e4c7
xyyx <xyyx@mail.ru>
2022-09-16 09:55:39 +0300

    charging: Allow using vibration without sounds

--- services/core/java/com/android/server/power/Notifier.java
+++ services/core/java/com/android/server/power/Notifier.java
@@ -828,9 +828,6 @@ public class Notifier {
     };
 
     private void playChargingStartedFeedback(@UserIdInt int userId, boolean wireless) {
-        if (!isChargingFeedbackEnabled(userId)) {
-            return;
-        }
 
         if (!mIsPlayingChargingStartedFeedback.compareAndSet(false, true)) {
             // there's already a charging started feedback Runnable scheduled to run on the
@@ -848,6 +845,7 @@ public class Notifier {
                         HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES);
             }
 
+        if (isChargingFeedbackEnabled(userId)) {
             // play sound
             final String soundPath = Settings.Global.getString(mContext.getContentResolver(),
                     wireless ? Settings.Global.WIRELESS_CHARGING_STARTED_SOUND
@@ -860,6 +858,7 @@ public class Notifier {
                     sfx.play();
                 }
             }
+        }
             mIsPlayingChargingStartedFeedback.set(false);
         });
     }
