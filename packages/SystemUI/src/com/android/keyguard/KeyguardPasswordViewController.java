02dc94c181dad3ea8909a3aec88e62881451ac97
xyyx <xyyx@mail.ru>
2022-12-06 12:43:20 +0000

    Fix QuickUnlock after android-13.0.0_r14

--- packages/SystemUI/src/com/android/keyguard/KeyguardPasswordViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPasswordViewController.java
@@ -60,6 +60,7 @@ public class KeyguardPasswordViewController
     private final InputMethodManager mInputMethodManager;
     private final DelayableExecutor mMainExecutor;
     private final KeyguardViewController mKeyguardViewController;
+    private final SecurityMode mSecurityMode;
     private final boolean mShowImeAtScreenOn;
     private EditText mPasswordEntry;
     private ImageView mSwitchImeButton;
@@ -136,6 +137,7 @@ public class KeyguardPasswordViewController
         mPasswordEntry = mView.findViewById(mView.getPasswordTextViewId());
         mSwitchImeButton = mView.findViewById(R.id.switch_ime_button);
         mLockPatternUtils = lockPatternUtils;
+        mSecurityMode = securityMode;
     }
 
     @Override
@@ -355,7 +357,7 @@ public class KeyguardPasswordViewController
     private void runQuickUnlock(Boolean matched) {
         if (matched) {
             mKeyguardSecurityCallback.reportUnlockAttempt(userId, true, 0);
-            mKeyguardSecurityCallback.dismiss(true, userId);
+            mKeyguardSecurityCallback.dismiss(true, userId, mSecurityMode);
             mView.resetPasswordText(true, true);
         }
     }
